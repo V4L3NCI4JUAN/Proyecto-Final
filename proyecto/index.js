cac28b9bf16e82e0095180316813c0e61030ae3a
Ferluispm28 <luisperesmarrugo28@gmail.com>
2023-12-03 18:36:20 -0500

    hola

--- proyecto/index.js
+++
@@ -1,7 +0,0 @@
-let contenido=document.getElementById('contenido');
-let contenido2=document.getElementById('contenido2');
-
-function enter(){
-    document.getElementById('contenido').style.display="none";
-    document.getElementById('contenido2').style.display="block";
-}
