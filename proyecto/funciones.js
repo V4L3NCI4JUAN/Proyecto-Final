18ef466334906efe994ad88d30b346458c049fd7
V4L3NCI4JUAN <juanjovalencia270@gmail.com>
2023-12-06 10:51:39 -0500

    muchos cambios

--- proyecto/funciones.js
+++ proyecto/funciones.js
@@ -5,3 +5,11 @@ function enter(){
     document.getElementById('contenido').style.display="none";
     document.getElementById('contenido2').style.display="block";
 }
+
+function dirijirAInicio(){
+    window.location.href="inicio.html";
+}
+
+function dirijirAQuieroTrabajar(){
+    window.location.href="quieroTrabajar.html";
+}
