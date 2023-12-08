74e32bf78ceca45e4d5d3c458610e7119531e304
V4L3NCI4JUAN <juanjovalencia270@gmail.com>
2023-12-08 00:43:34 -0500

    wdw

--- proyecto/funciones.js
+++ proyecto/funciones.js
@@ -1,29 +1,51 @@
 let contenido = document.getElementById('contenido');
 let contenido2 = document.getElementById('contenido2');
 let contenido_ventana = document.getElementById('contenido_ventana');
-
 function enter(){
     document.getElementById('contenido').style.display="none";
     document.getElementById('contenido2').style.display="block";
 }
-
 function dirijirAInicio(){
     window.location.href="inicio.html";
 }
 function dirijirAQuieroTrabajar(){
     window.location.href="quieroTrabajar.html";
 }
-
 function desplegarVentana(){
     document.getElementById('contenido_ventana').style.display='block';
 }
 function cerrarVentana(){
     document.getElementById('contenido_ventana').style.display='none';
 }
-
 function IraRegistrar(){
     window.location.href="index.html";
 }
-function agregarExperiencia(){
-    document.createElement("input");
+
+function enviar(){
+    const nombre = document.getElementById("nombre").value;
+    const primerApellido = document.getElementById("primerApellido").value;
+    const segundoApellido = document.getElementById("segundoApellido").value;
+    const tipoDocumento = document.getElementById("tipoDocumento").value;
+    const documento = document.getElementById("documento").value;
+    const estadoCivil = document.getElementById("estadoCivil").value;
+    const celular = document.getElementById("celular").value;
+    const correo = document.getElementById("correo").value;
+    const paisNacimiento = document.getElementById("paisNacimiento").value;
+    const departamentoNacimiento = document.getElementById("departamentoNacimiento").value;
+    const ciudadNacimiento = document.getElementById("ciudadNacimiento").value;
+    const fechaNacimiento = document.getElementById("fechaNacimiento").value;
+    const paisResidencia = document.getElementById("paisResidencia").value;
+    const departamentoResidencia = document.getElementById("departamentoResidencia").value;
+    const ciudadResidencia = document.getElementById("ciudadResidencia").value;
+    const direccion = document.getElementById("direccion").value;
+    const barrio = document.getElementById("barrio").value;
+    const cargo = document.getElementById("cargo").value;
+
+    const datosPersonales = JSON.parse(localStorage.getItem("datosPersonales"))||[];
+    datosPersonales.push({nombre,primerApellido,segundoApellido,tipoDocumento,documento,estadoCivil,celular,correo,
+    paisNacimiento,departamentoNacimiento,ciudadNacimiento,fechaNacimiento,paisResidencia,departamentoResidencia,
+    ciudadResidencia,direccion,barrio,cargo});
+
+    localStorage.setItem("datosPersonales", JSON.stringify(datosPersonales));
+    alert("Datos enviados exitosamente");
 }
